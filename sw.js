7f68624d4dfc407ec47fa8b4097bed8ccecc3ef1
sanuka18 <sanu.ratnayake@gmail.com>
2024-09-13 07:28:18 +0530

    {second}

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-            '/Katsura/',  // Update this path
-            '/Katsura/index.html',  // Update this path
-            '/Katsura/styles/styles.css',  // Update this path
-            '/Katsura/scripts/script.js',  // Update this path
-            '/Katsura/favicons/favicon-192x192.png',  // Update this path
-            '/Katsura/favicons/favicon-512x512.png'  // Update this path
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+          '/sanuka/',  // Update this path
+          '/sanuka/index.html',  // Update this path
+          '/sanuka/styles/style.css',  // Update this path
+          '/sanuka/scripts/script.js',  // Update this path
+          '/sanuka/favicons/favicon-192x192.png',  // Update this path
+          '/sanuka/favicons/favicon-512x512.png'  // Update this path
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
